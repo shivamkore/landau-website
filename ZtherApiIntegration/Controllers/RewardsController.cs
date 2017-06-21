2918de4f2cf2c8eb26fefc8727fb7db1587df19d
AJ <arsenio.macapaz@landau.com>
2017-06-21 11:24:24 -0500

    Added Rewards page.

---
+++ ZtherApiIntegration/Controllers/RewardsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZtherApiIntegration.Controllers
+{
+    public class RewardsController : Controller
+    {
+
+        [Route("rewards")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
