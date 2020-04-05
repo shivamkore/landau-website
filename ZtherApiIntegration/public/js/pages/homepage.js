4501b645f5588748021a0675ed622107b55fc8cb
AJ Macapaz <arsenio.macapaz@outlook.com>
2020-04-04 20:26:43 -0500

    Adding Vimeo video replacing Wistia.

--- ZtherApiIntegration/public/js/pages/homepage.js
+++ ZtherApiIntegration/public/js/pages/homepage.js
@@ -1,7 +1,32 @@
-$(function() {
+$(function () {
+
+    // Vimeo API nonsense
+    //var player = document.getElementById('player_1');
+    //$f(player).addEvent('ready', ready);
+
+    //function addEvent(element, eventName, callback) {
+    //    if (element.addEventListener) {
+    //        element.addEventListener(eventName, callback, false)
+    //    } else {
+    //        element.attachEvent(eventName, callback, false);
+    //    }
+    //}
+
+    //function ready(player_id) {
+    //    var froogaloop = $f(player_id);
+    //    froogaloop.addEvent('play', function (data) {
+    //        $('.flexslider').flexslider("pause");
+    //    });
+    //    froogaloop.addEvent('pause', function (data) {
+    //        $('.flexslider').flexslider("play");
+    //    });
+    //}
+
     // FlexSlider Homepage
-    $('#homepage .home_slider .flexslider').flexslider({
-        slideshow: true,
+    $('#homepage .home_slider .flexslider')
+        //.fitVids()
+        .flexslider({
+        slideshow: false,
         animation: "fade",
         slideshowSpeed: 7000,
         animationSpeed: 650,
